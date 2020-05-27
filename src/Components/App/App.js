cfcd29b3205d976fc5e75fff1a76a60eb20d4dce
onTheDL <daniel.laserna@protonmail.com>
2020-05-27 16:58:47 -0400

    Prop array from App.js not defined/inherited in SearchResults.js

---
+++ src/Components/App/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import Playlist from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      searchResults: [
+        {name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
+        {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
+        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+      ] 
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* <!-- Add a SearchBar component --> */}
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component --> */}
+            <SearchResults searchResults={this.state.searchResults} />
+            {/* <!-- Add a Playlist component --> */}
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
